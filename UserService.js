fb2c15566a68cd814c403a1b09a2a706d391cac4
Nina Liljeblad <nina.liljeblad@gmail.com>
2016-02-18 22:06:49 +0200

    dynamic content

---
+++ UserService.js
@@ -0,0 +1,31 @@
+(function(){
+'use strict'
+
+angular.module('users')
+.service('userService', UserService);
+
+function UserService($q){
+	var users =[
+	{
+	name: 'Amber AAle',
+	avatar: 'Amber',
+	content: 'Amber Ale is reddish and oh, so GOOD'
+	},
+	{
+	name: 'Stout',
+	avatar: 'Stout',
+	content: 'Stout is dark and oh, so GOOD'
+	},
+
+	];
+	 // Promise-based API
+  return {
+    loadAllUsers : function() {
+      // Simulate async nature of real remote calls
+      return $q.when(users);
+    }
+  };
+}
+
+ 
+  })();
